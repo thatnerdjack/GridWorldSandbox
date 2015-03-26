4f39f025568a45ad3d1ddc9ef20ca5a282591bbc
thatnerdjack <jackjyro@gmail.com>
2015-03-26 13:20:52 -0400

    created gridRacer project

---
+++ src/com/github/thatnerdjack/gridworldsandbox/gridRacer/GridRacerRunner.java
@@ -0,0 +1,12 @@
+package com.github.thatnerdjack.gridworldsandbox.gridRacer;
+
+/**
+ * Created by block7 on 3/26/15.
+ */
+public class GridRacerRunner {
+
+    public static void main(String args[]) {
+        //code here
+    }
+
+}
---
+++ src/com/github/thatnerdjack/gridworldsandbox/gridRacer/TrackObject.java
@@ -0,0 +1,7 @@
+package com.github.thatnerdjack.gridworldsandbox.gridRacer;
+
+/**
+ * Created by block7 on 3/26/15.
+ */
+public abstract class TrackObject {
+}
