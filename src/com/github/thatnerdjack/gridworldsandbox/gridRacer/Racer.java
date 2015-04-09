475e1af0ee668eeffe5390b3b922d1b520e1b55b
thatnerdjack <jackjyro@gmail.com>
2015-04-09 13:40:12 -0400

    working on stuff

--- src/com/github/thatnerdjack/gridworldsandbox/gridRacer/Racer.java
+++ src/com/github/thatnerdjack/gridworldsandbox/gridRacer/Racer.java
@@ -1,6 +1,11 @@
 package com.github.thatnerdjack.gridworldsandbox.gridRacer;
 
+import info.gridworld.actor.Actor;
 import info.gridworld.actor.Critter;
+import info.gridworld.grid.Location;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * Created by block7 on 3/26/15.
@@ -11,6 +16,7 @@ public class Racer extends Critter {
 
     private int health = 100;
     private int fuel = 100;
+    private int movingDirection = Location.WEST;
 
     public Racer() {
         acceleration = (int)(Math.random() * 10);
@@ -22,9 +28,25 @@ public class Racer extends Critter {
         this.handling = handling;
     }
 
-//    public void act() {
-//        Location loc = getLocation();
-//        loc.getAdjacentLocation(Location.WEST);
-//        makeMove(loc);
-//    }
+    public void act() {
+        if(this.getGrid() != null) {
+            ArrayList actors = this.getActors();
+            this.processActors(actors);
+            Location loc = this.getLocation();
+            Location moveLoc = loc.getAdjacentLocation(movingDirection);
+            this.makeMove(moveLoc);
+        }
+    }
+
+    public void processActors(ArrayList<Actor> actors) {
+        Iterator i$ = actors.iterator();
+
+        while(i$.hasNext()) {
+            Actor a = (Actor)i$.next();
+            if(!(a instanceof Obstacle) && !(a instanceof Racer)) {
+                a.removeSelfFromGrid();
+            }
+        }
+
+    }
 }
